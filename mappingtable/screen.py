4cb05918bdbed427d430eefba6ab5d6a9bb6bd67
penrin <hrs@penr.in>
2019-07-17 11:31:06 +0900

    add function add_equirectangular_margin() at screen.py

--- mappingtable/screen.py
+++ mappingtable/screen.py
@@ -78,3 +78,36 @@ def set_config(path2work):
     
 
 
+
+def add_equirectangular_margin(img, margin_x, margin_y=None):
+    
+    if margin_y == None:
+        margin_y = margin_x
+    if (type(margin_x) != int) or (type(margin_y) != int):
+        print('The decimal point of margins are rounded up.')
+        margin_x = int(np.ceil(margin_x))
+        margin_y = int(np.ceil(margin_y))
+    if img.ndim != 3:
+        raise Exception('image dimention shoud be 3.')
+    if (margin_y > img.shape[0]) or (margin_x > img.shape[1]):
+        raise Exception('The size of the margin is up to the image size.')
+    
+    mx = margin_x
+    my = margin_y
+    H = img.shape[0] + margin_y * 2
+    W = img.shape[1] + margin_x * 2
+    D = img.shape[2]
+    img_ = np.empty([H, W, D], dtype=img.dtype)
+    img_[my:-my, mx:-mx, :] = img
+    img_[:my, mx:W // 2 ,:] = img_[my:my * 2, W // 2:-mx, :][::-1]
+    img_[:my, W // 2:-mx ,:] = img_[my:my * 2, mx:W // 2, :][::-1]
+    img_[-my:, mx:W // 2, :] = img_[-my * 2:-my, W // 2:-mx, :][::-1]
+    img_[-my:, W // 2:-mx, :] = img_[-my * 2:-my, mx:W // 2, :][::-1]
+    img_[:, :mx, :] = img_[:, -mx * 2:-mx, :]
+    img_[:, -mx:, :] = img_[:, mx:mx * 2, :]
+    return img_
+
+
+
+if __name__ == '__main__':
+    pass
